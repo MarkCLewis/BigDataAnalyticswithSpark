a4e3d94ace8aff5b687beb797d9155b27130b83a
Mark Lewis <mlewis@trinity.edu>
2017-09-29 08:09:55 -0500

    Plotting example completed for Spark SQL.

--- src/main/scala/sparksql/NOAAData.scala
+++ src/main/scala/sparksql/NOAAData.scala
@@ -7,6 +7,10 @@ import org.apache.spark.sql.types.StructField
 import org.apache.spark.sql.types.StringType
 import org.apache.spark.sql.types.DateType
 import org.apache.spark.sql.types.DoubleType
+import org.apache.spark.sql.Row
+import org.apache.spark.sql.functions._
+import swiftvis2.plotting._
+import swiftvis2.plotting.renderer.FXRenderer
 
 object NOAAData extends JFXApp {
   val spark = SparkSession.builder().master("local[*]").appName("NOAA Data").getOrCreate()
@@ -20,15 +24,40 @@ object NOAAData extends JFXApp {
       StructField("mtype",StringType),
       StructField("value",DoubleType)
       ))
-  val data2017 = spark.read.schema(tschema).option("dateFormat", "yyyyMMdd").csv("data/2017.csv")
+  val data2017 = spark.read.schema(tschema).option("dateFormat", "yyyyMMdd").csv("data/2017.csv").cache()
 //  data2017.show()
 //  data2017.schema.printTreeString()
   
-  val tmax2017 = data2017.filter($"mtype" === "TMAX").limit(1000).drop("mtype").withColumnRenamed("value", "tmax")
-  val tmin2017 = data2017.filter('mtype === "TMIN").limit(1000).drop("mtype").withColumnRenamed("value", "tmin")
+  val sschema = StructType(Array(
+      StructField("sid", StringType),
+      StructField("lat", DoubleType),
+      StructField("lon", DoubleType),
+      StructField("name", StringType)
+      ))
+  val stationRDD = spark.sparkContext.textFile("data/ghcnd-stations.txt").map { line =>
+    val id = line.substring(0, 11)
+    val lat = line.substring(12, 20).toDouble
+    val lon = line.substring(21, 30).toDouble
+    val name = line.substring(41, 71)
+    Row(id, lat, lon, name)
+  }
+  val stations = spark.createDataFrame(stationRDD, sschema).cache()
+  
+  val tmax2017 = data2017.filter($"mtype" === "TMAX").limit(1000000).drop("mtype").withColumnRenamed("value", "tmax")
+  val tmin2017 = data2017.filter('mtype === "TMIN").limit(1000000).drop("mtype").withColumnRenamed("value", "tmin")
   val combinedTemps2017 = tmax2017.join(tmin2017, Seq("sid", "date"))
-  val averageTemp2017 = combinedTemps2017.select('sid, 'date, ('tmax + 'tmin)/2)
-  averageTemp2017.show()
+  val dailyTemp2017 = combinedTemps2017.select('sid, 'date, ('tmax + 'tmin)/20*1.8+32).withColumnRenamed("((((tmax + tmin) / 20) * 1.8) + 32)", "tave")
+  val stationTemp2017 = dailyTemp2017.groupBy('sid).agg(avg('tave))
+  val joinedData2017 = stationTemp2017.join(stations, "sid")
+  val localData = joinedData2017.collect()
+  
+  val temps = localData.map(_.getDouble(1))
+  val lats = localData.map(_.getDouble(2))
+  val lons = localData.map(_.getDouble(3))
+  val cg = ColorGradient(0.0 -> BlueARGB, 50.0 -> GreenARGB, 100.0 -> RedARGB)
+  val plot = Plot.scatterPlot(lons, lats, "Global Temps", "Longitude", "Latitude",
+      3, temps.map(cg))
+  FXRenderer(plot, 800, 600)
   
   spark.stop()
 }

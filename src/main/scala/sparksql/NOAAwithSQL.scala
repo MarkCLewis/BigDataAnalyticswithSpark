1041c6411aa6651ce8e573156f5c9140484982f7
Mark Lewis <mlewis@trinity.edu>
2017-10-01 14:16:11 -0500

    Finished off the SQL part of Spark SQL and then finished the sample offline giving it full functionality.

--- src/main/scala/sparksql/NOAAwithSQL.scala
+++ src/main/scala/sparksql/NOAAwithSQL.scala
@@ -1,39 +1,36 @@
 package sparksql
 
-import org.apache.spark.sql.SparkSession
 import scalafx.application.JFXApp
+import org.apache.spark.sql.types.DoubleType
+import org.apache.spark.sql.types.DateType
+import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.types.StructType
 import org.apache.spark.sql.types.StructField
 import org.apache.spark.sql.types.StringType
-import org.apache.spark.sql.types.DateType
-import org.apache.spark.sql.types.DoubleType
 import org.apache.spark.sql.Row
-import org.apache.spark.sql.functions._
 import swiftvis2.plotting._
 import swiftvis2.plotting.renderer.FXRenderer
 
-object NOAAData extends JFXApp {
+object NOAAwithSQL extends JFXApp {
   val spark = SparkSession.builder().master("local[*]").appName("NOAA Data").getOrCreate()
   import spark.implicits._
-  
+
   spark.sparkContext.setLogLevel("WARN")
-  
+
   val tschema = StructType(Array(
-      StructField("sid",StringType),
-      StructField("date",DateType),
-      StructField("mtype",StringType),
-      StructField("value",DoubleType)
-      ))
+    StructField("sid", StringType),
+    StructField("date", DateType),
+    StructField("mtype", StringType),
+    StructField("value", DoubleType)))
   val data2017 = spark.read.schema(tschema).option("dateFormat", "yyyyMMdd").csv("data/2017.csv").cache()
-//  data2017.show()
-//  data2017.schema.printTreeString()
-  
+  //  data2017.show()
+  //  data2017.schema.printTreeString()
+
   val sschema = StructType(Array(
-      StructField("sid", StringType),
-      StructField("lat", DoubleType),
-      StructField("lon", DoubleType),
-      StructField("name", StringType)
-      ))
+    StructField("sid", StringType),
+    StructField("lat", DoubleType),
+    StructField("lon", DoubleType),
+    StructField("name", StringType)))
   val stationRDD = spark.sparkContext.textFile("data/ghcnd-stations.txt").map { line =>
     val id = line.substring(0, 11)
     val lat = line.substring(12, 20).toDouble
@@ -42,22 +39,29 @@ object NOAAData extends JFXApp {
     Row(id, lat, lon, name)
   }
   val stations = spark.createDataFrame(stationRDD, sschema).cache()
-  
-  val tmax2017 = data2017.filter($"mtype" === "TMAX").limit(1000000).drop("mtype").withColumnRenamed("value", "tmax")
-  val tmin2017 = data2017.filter('mtype === "TMIN").limit(1000000).drop("mtype").withColumnRenamed("value", "tmin")
-  val combinedTemps2017 = tmax2017.join(tmin2017, Seq("sid", "date"))
-  val dailyTemp2017 = combinedTemps2017.select('sid, 'date, ('tmax + 'tmin)/20*1.8+32 as "tave")
-  val stationTemp2017 = dailyTemp2017.groupBy('sid).agg(avg('tave))
-  val joinedData2017 = stationTemp2017.join(stations, "sid")
-  val localData = joinedData2017.collect()
-  
+
+  data2017.createOrReplaceTempView("data2017")
+  stations.createOrReplaceTempView("stations")
+  val pureSQL = spark.sql("""
+    Select * FROM
+      (SELECT sid, AVG((tmax+tmin)/20*1.8+32) as tave
+      FROM
+        (SELECT sid, date, value as tmax FROM data2017 WHERE mtype="TMAX" LIMIT 1000000)
+        JOIN
+        (SELECT sid, date, value as tmin FROM data2017 WHERE mtype="TMIN" LIMIT 1000000)
+        USING (sid, date)
+      GROUP BY sid)
+    JOIN stations USING (sid)
+    """)
+  pureSQL.show()
+
+  val localData = pureSQL.collect()
   val temps = localData.map(_.getDouble(1))
   val lats = localData.map(_.getDouble(2))
   val lons = localData.map(_.getDouble(3))
   val cg = ColorGradient(0.0 -> BlueARGB, 50.0 -> GreenARGB, 100.0 -> RedARGB)
   val plot = Plot.scatterPlot(lons, lats, "Global Temps", "Longitude", "Latitude",
-      3, temps.map(cg))
+    3, temps.map(cg))
   FXRenderer(plot, 800, 600)
-  
-  spark.stop()
+
 }
